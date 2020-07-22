e8de51939ffe53c3d6890815f2762b9c7de96b1d
xuan <2250851079@qq.com>
2020-07-22 10:30:57 +0800

    创建后台主页

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,14 +1,23 @@
 <template>
-    <div>
-        <el-button type="info" @click="logout">退出</el-button>
-    </div>
+<el-container>
+    <!-- 注意：每一个 el-container 中提供的组件 其组件名就是类名(可以直接做样式修饰) -->
+    <!-- 头部区域 -->
+    <el-header>Header<el-button type="info" @click="logout">退出</el-button>
+    </el-header>
+    <!-- 页面主体区域 -->
+    <el-container>
+        <!-- 侧边栏 -->
+        <el-aside width="200px">Aside</el-aside>
+        <el-main>Main</el-main>
+    </el-container>
+</el-container>
 </template>
 
 <script>
 export default {
-    methods:{
+    methods: {
         // 退出登录
-        logout(){
+        logout() {
             // 清空token
             window.sessionStorage.clear();
             // 跳转到登录页
@@ -19,5 +28,18 @@ export default {
 </script>
 
 <style lang="less" scoped>
+.el-container {
+    height: 100%;
+}
+.el-header {
+    background-color: #373d41;
+}
 
+.el-aside {
+    background-color: #333744;
+}
+
+.el-main {
+    background-color: #eaedf1;
+}
 </style>
--- src/plugins/element.js
+++ src/plugins/element.js
@@ -5,11 +5,18 @@ import { Form, FormItem } from 'element-ui'
 import { Input } from 'element-ui'
 // 导入弹框提示组件
 import { Message } from 'element-ui'
+// 导入布局容器组件
+import { Container, Header, Aside, Main, Menu } from 'element-ui'
 
 Vue.use(Button)
 Vue.use(Form)
 Vue.use(FormItem)
 Vue.use(Input)
+Vue.use(Container)
+Vue.use(Header)
+Vue.use(Aside)
+Vue.use(Main)
+Vue.use(Menu)
 
 // 将弹框组件挂载到Vue原型对象身上 这样就可以在所有Vue组件中通过this.$message(自定义的)进行弹框提示
 Vue.prototype.$message = Message
